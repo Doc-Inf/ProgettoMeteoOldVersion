ebf9167f62a5fe85be64385989e0b6296f8e365e
Ace-69 <50658429+Ace-69@users.noreply.github.com>
2022-12-13 22:08:40 +0100

    Aggiunto template per database

---
+++ template.sql
@@ -0,0 +1,16 @@
+/* Creazione database meteo */
+CREATE SCHEMA 'meteo';
+
+/* Template provvisorio per le tabelle 
+ * c'è da sperimentare con i tempi di esecuzione delle query
+ */
+CREATE TABLE `2022` (
+  `id` int(11) NOT NULL,
+  `data` datetime DEFAULT NULL,
+  `temperatura` int(11) DEFAULT NULL,
+  `pressione` double DEFAULT NULL,
+  `umidita` int(11) DEFAULT NULL,
+  `direzione-vento` varchar(2) DEFAULT NULL,
+  `km-h` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
