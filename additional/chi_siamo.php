e4dc01bc4dc8c7c26b5097d7290f2f9080ba96e0
Lorenzoboar <120569374+Lorenzoboar@users.noreply.github.com>
2023-04-19 20:16:59 +0200

    Sistemati i principali problemi
    completato il grafico
    risolto css "admin"
    aggiunte img a "chiSiamo"

--- additional/chi_siamo.php
+++ additional/chi_siamo.php
@@ -32,14 +32,13 @@
         ?>
     </nav>
 
-    <h2 class="benvenuto noiSiamo spazio">
+    <h2 class="benvenuto noiSiamo">
         Noi siamo ragazzi dell'ITIS Vallauri Velletri.
     </h2>
 
     <h2 class="benvenuto noiSiamo">
         Questo è un progetto elaborato da 3 classi della scuola per illustrare le competenze acquisite.
     </h2>
-    
 
     <!-- mappa -->
 
@@ -51,49 +50,54 @@
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2980.0416283289383!2d12.775315615372184!3d41.676444379238745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13259d347813ad7d%3A0x973948516bf1e19f!2sITIS%20Giancarlo%20Vallauri!5e0!3m2!1sit!2sit!4v1678190626853!5m2!1sit!2sit" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 
-
     <div class="spazio sfondotrasparente">
-        <h2 class="benvenuto">
-            La Storia della scuola
-        </h2>
-        <hr>
-        <h2 class="benvenuto">
-            La scuola viene istituita nell' A. S. 1960 /61 come succursale dell'I.T.I.S. "E. Fermi" di Roma all'epoca esistevano solo tre sezioni: la sezione A di 37 alunni la sezione B di 37 alunni la sezione C di 39 alunni le quali erano seguite dal Prof. Diana Umberto. il quale è stato il coordinatore con la partecipazione dell' I.T.I.S. E. Fermi di Roma per l' avvio e la gestione iniziale della scuola. Tutti i comuni a sud di Roma come ad esempio i Castelli , Colleferro ecc. insieme ad alcuni comuni della provincia di Latina, fino a giungere a Terracina, rappresentano il bacino d 'utenza dell' attuale I.T.I.S. "Giancarlo Vallauri" di Velletri. Inizialmente l'Istituto si appoggiò presso l' I.T.C.G. "Cesare Battisti" , per passare poi dal palazzo comunale ad una villetta non troppo distante l'ospedale di Velletri. Nel mese di novembre dell'anno 1965 l'I.T.I.S. Vallauri, si trasferì definitivamente presso la sede attuale di via Salvo D' Acquisto. Nell' A. S. 1968 / 69 divenne istituto autonomo e prese per l'appunto il nome dell' ingegnere "Giancarlo Vallauri". Dalla sua autonomia ad oggi, più di cinquemila sono stati gli studenti diplomati. Questo perché il numero delle sezioni e degli alunni è progressivamente cresciuto nel corso del tempo; fino a raggiungere, un numero complessivo di 53 classi, 14 di esse sono state ospitate nella sede staccata di via Paolina a Velletri. La scuola dispone inoltre di diversi laboratori, dove gli alunni possono predisporre numerose esperienze. Al tempo stesso, dopo l'entrata in vigore dei bacini di utenza, (DISTRETTO N°39/42) si è precisata e definita l'area geografica dell'I.T.I.S. "G. Vallauri": gli allievi provengono ,infatti, prevalentemente da Lariano, Genzano, Albano, Ariccia, Lanuvio, Cecchina, S.Maria delle Mole, Castel Gandolfo, Ciampino, Cisterna e Artena oltre che , come ovvio, dalla stessa Velletri. Alla iniziale specializzazione in Elettronica Industriale si sono affiancati, più di recente nuovi corsi (INFORMATICA e LICEO SCIENTIFICO TECNOLOGICO), ciò nell'intento di rispondere con sempre maggiore efficacia alla domanda e alle legittime esigenze del mondo del lavoro che, alla scuola chiede un continuo sforzo di aggiornamento per adeguare i futuri periti all'apprendimento e all'uso delle nuove tecnologie.
-        </h2>
+        <div class="sottoSpazio1">
+            <h2 class="benvenuto">
+                La Storia della scuola
+            </h2>
+            <hr>
+            <h2 class="storia">
+                La scuola viene istituita nell' A. S. 1960 /61 come succursale dell'I.T.I.S. "E. Fermi" di Roma all'epoca esistevano solo tre sezioni: la sezione A di 37 alunni la sezione B di 37 alunni la sezione C di 39 alunni le quali erano seguite dal Prof. Diana Umberto. il quale è stato il coordinatore con la partecipazione dell' I.T.I.S. E. Fermi di Roma per l' avvio e la gestione iniziale della scuola. Tutti i comuni a sud di Roma come ad esempio i Castelli , Colleferro ecc. insieme ad alcuni comuni della provincia di Latina, fino a giungere a Terracina, rappresentano il bacino d 'utenza dell' attuale I.T.I.S. "Giancarlo Vallauri" di Velletri. Inizialmente l'Istituto si appoggiò presso l' I.T.C.G. "Cesare Battisti" , per passare poi dal palazzo comunale ad una villetta non troppo distante l'ospedale di Velletri. Nel mese di novembre dell'anno 1965 l'I.T.I.S. Vallauri, si trasferì definitivamente presso la sede attuale di via Salvo D' Acquisto. Nell' A. S. 1968 / 69 divenne istituto autonomo e prese per l'appunto il nome dell' ingegnere "Giancarlo Vallauri". Dalla sua autonomia ad oggi, più di cinquemila sono stati gli studenti diplomati. Questo perché il numero delle sezioni e degli alunni è progressivamente cresciuto nel corso del tempo; fino a raggiungere, un numero complessivo di 53 classi, 14 di esse sono state ospitate nella sede staccata di via Paolina a Velletri. La scuola dispone inoltre di diversi laboratori, dove gli alunni possono predisporre numerose esperienze. Al tempo stesso, dopo l'entrata in vigore dei bacini di utenza, (DISTRETTO N°39/42) si è precisata e definita l'area geografica dell'I.T.I.S. "G. Vallauri": gli allievi provengono ,infatti, prevalentemente da Lariano, Genzano, Albano, Ariccia, Lanuvio, Cecchina, S.Maria delle Mole, Castel Gandolfo, Ciampino, Cisterna e Artena oltre che , come ovvio, dalla stessa Velletri. Alla iniziale specializzazione in Elettronica Industriale si sono affiancati, più di recente nuovi corsi (INFORMATICA e LICEO SCIENTIFICO TECNOLOGICO), ciò nell'intento di rispondere con sempre maggiore efficacia alla domanda e alle legittime esigenze del mondo del lavoro che, alla scuola chiede un continuo sforzo di aggiornamento per adeguare i futuri periti all'apprendimento e all'uso delle nuove tecnologie.
+            </h2>
+        </div>
+        <div class="sottoSpazio2">
+            <img src="../img/scuola1.jpg" alt="IMG-scuola">
+            <img src="../img/scuola2.jpg" alt="IMG-scuola">
+        </div>
     </div>
 
-    <div class="spazio sfondotrasparente">
+    <div class="spazio">
         <h2 class="benvenuto"> 
             Partecipanti al Progetto
         </h2>
         <hr>
-        <h2 class="benvenuto" style="color:yellowgreen;">
+        <h5 class="benvenuto" style="color:#00FF00;">
             Somma Francesco [Back-End, Front-End, Database]
-        </h2>
+        </h5>
         <br>
-        <h2 class="benvenuto" style="color:yellowgreen;">
+        <h5 class="benvenuto" style="color:#00FF00;">
             Fonti Luca [Back-End, Front-End, Database]
-        </h2>
+        </h5>
         <br>
-        <h2 class="benvenuto" style="color:yellowgreen;">     
+        <h5 class="benvenuto" style="color:#00FF00;">     
             Boaretto Lorenzo [Back-End, Front-End, Grafica]
-        </h2>
+        </h5>
         <br>
-        <h2 class="benvenuto" style="color:khaki;">
+        <h5 class="benvenuto" style="color:#0000FF;">
             Cipolla Emilio [Front-end, Grafica]
-        </h2>
+        </h5>
         <br>
-        <h2 class="benvenuto" style="color:khaki;">
+        <h5 class="benvenuto" style="color:#0000FF;">
             Pietrosanti Francesco [Front-End, Grafica, Presentazione]
-        </h2>
+        </h5>
         <br>
-        <h2 class="benvenuto" style="color:#00000055; text-shadow: 0 0 #00000000;">
+        <h5 class="benvenuto" style="color:#FF0000;">
             Imbastari Riccardo [Grafica, Presentazione]
-        </h2>
+        </h5>
         <br>
-        <h2 class="benvenuto" style="color:#00000055; text-shadow: 0 0 #00000000;">
+        <h5 class="benvenuto" style="color:#FF0000;">
             Fruncillo Carmine [Idee]
-        </h2>
+        </h5>
     </div>
 
 </body>
--- additional/storico.php
+++ additional/storico.php
@@ -56,102 +56,227 @@
             
         </div>
     </div>
-    
+
     <?php
-        if($_POST) {
-        
-            $date = new DateTime($_POST["dataIn"]);
-            $year = $date->format("Y");
-            
-            $res = query("SELECT * FROM `$year` WHERE data BETWEEN '".$date->format('Y/m/d')." 00:00:00' AND '".$date->format('Y/m/d')." 23:59:59' ORDER BY data desc;" );
-            $res = $res[0];
-            
 
-            function cielo($res) { // $res MUST be an array containing numeric values labeled as "umidita" and "temperatura"
-                if($res["umidita"] >= 90 && $res["temperatura"] <= 15) {
-                    return "Pioggia";
-                } else if($res["umidita"] <= 85 && $res["umidita"] >= 75 && $res["temperatura"] <= 21) {
-                    return "Cielo Nuvoloso";
-                } else if($res["umidita"] <= 50 ) {
-                    return "Cielo Soleggiato";
-                } else {
-                    return "Cielo Parzialmente nuvoloso";
-                }
+    if($_POST) {
+            
+        function cielo($res) { // $res MUST be an array containing numeric values labeled as "umidita" and "temperatura"
+            if($res["umidita"] >= 90 && $res["temperatura"] <= 15) {
+                return "Pioggia";
+            } else if($res["umidita"] <= 85 && $res["umidita"] >= 75 && $res["temperatura"] <= 21) {
+                return "Cielo Nuvoloso";
+            } else if($res["umidita"] <= 50 ) {
+                return "Cielo Soleggiato";
+            } else {
+                return "Cielo Parzialmente nuvoloso";
             }
+        }
 
+        $date = new DateTime($_POST["dataIn"]);
+        $year = $date->format("Y");
+        
+        $date->setTime(23,9,4); // set time at last measure for day Hour:minute:second 
+        $sql = "SELECT * FROM `Y$year` WHERE DATE(data)='".$date->format('Y-m-d')."';";           
+        $res = $db->query($sql);            
+        
+
+        if( $res && isset($res[0]['data'])) {     
+            $res = $res[0];
             echo '<div class="page" id="zona"> 
                     <div class="pacchetto">
                         <div class="info" id="infolarghezza1">
                             <h3 class="bordo">Misurazione giornaliera</h3>
+                            <h3 class="bordo">Data: '. extractDate($res['data']) .'</h3>
                             <h3 class="bordo">'.cielo($res).'</h3>
-                            <h3 class="bordo">Gradi:'.$res['temperatura'].'°</h3>
-                            <h3 class="bordo">Umidita:'.$res['umidita'].'%</h3>
+                            <h3 class="bordo">Gradi: '.$res['temperatura'].'°</h3>
+                            <h3 class="bordo">Umidita: '.$res['umidita'].'%</h3>
                             <h3 class="bordo">Pressione: '.$res['pressione'].' hPa</h3>
                             <h3 class="bordo">Direzione vento: '.$res['direzione-vento'].'</h3>
                             <h3 class="bordo">Velocità del vento: '.$res['km-h'].' Km/h</h3>
-                            <h3>Misurazione n. '.$res['id'].'<h3>
-                        </div>
-                        <h6>Data e ora misurazione: '.$res['data'].'</h6>
+                            <h3>Misurazione n. '.$res['id'].'<h3>                            
+                        </div>                        
                     </div>
                 </div>';
-
-            $monthData = query("SELECT DISTINCT DATE(data) as t, temperatura, umidita, pressione, `direzione-vento`, `km-h`, id, data from `$year` WHERE MONTH(data)= ".$date->format('m')." AND YEAR(data)=".$year." GROUP BY t ORDER BY data;");
-                        
-            //print chart
-            echo '<div class="chartTest" style="height: 50%; width: 70%; margin:auto;">
-                    <div id="curve_chart" class="grafico"></div>
+        }else{
+            echo '<div class="page" id="zona"> 
+                    <div class="pacchetto">
+                        <div class="info" id="infolarghezza1">
+                            <h3 class="bordo">Misurazione giornaliera non disponibile</h3>
+                            <h3 class="bordo">Data: ' . $date->format("d-m-Y") . '</h3>
+                            <h3 class="bordo">Gradi: NA</h3>
+                            <h3 class="bordo">Umidita: NA</h3>
+                            <h3 class="bordo">Pressione: NA</h3>
+                            <h3 class="bordo">Direzione vento: NA</h3>
+                            <h3 class="bordo">Velocità del vento: NA</h3>
+                            <h3>Misurazione n. NA<h3>                            
+                        </div>                        
+                    </div>
                 </div>';
         }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    if($_POST) {
+
+        $date = new DateTime($_POST["dataIn"]);
+        $year = $date->format("Y");
+        
+        $res = query("SELECT * FROM `$year` WHERE data BETWEEN '".$date->format('Y/m/d')." 00:00:00' AND '".$date->format('Y/m/d')." 23:59:59' ORDER BY data desc;" );
+        $res = $res[0];
+        
+
+        function cielo($res) { // $res MUST be an array containing numeric values labeled as "umidita" and "temperatura"
+            if($res["umidita"] >= 90 && $res["temperatura"] <= 15) {
+                return "Pioggia";
+            } else if($res["umidita"] <= 85 && $res["umidita"] >= 75 && $res["temperatura"] <= 21) {
+                return "Cielo Nuvoloso";
+            } else if($res["umidita"] <= 50 ) {
+                return "Cielo Soleggiato";
+            } else {
+                return "Cielo Parzialmente nuvoloso";
+            }
+        }
+
+        echo '<div class="page" id="zona"> 
+                <div class="pacchetto">
+                    <div class="info" id="infolarghezza1">
+                        <h3 class="bordo">Misurazione giornaliera</h3>
+                        <h3 class="bordo">'.cielo($res).'</h3>
+                        <h3 class="bordo">Gradi:'.$res['temperatura'].'°</h3>
+                        <h3 class="bordo">Umidita:'.$res['umidita'].'%</h3>
+                        <h3 class="bordo">Pressione: '.$res['pressione'].' hPa</h3>
+                        <h3 class="bordo">Direzione vento: '.$res['direzione-vento'].'</h3>
+                        <h3 class="bordo">Velocità del vento: '.$res['km-h'].' Km/h</h3>
+                        <h3>Misurazione n. '.$res['id'].'<h3>
+                    </div>
+                    <h6>Data e ora misurazione: '.$res['data'].'</h6>
+                </div>
+            </div>';
+
+        $monthData = query("SELECT DISTINCT DATE(data) as t, temperatura, umidita, pressione, `direzione-vento`, `km-h`, id, data from `$year` WHERE MONTH(data)= ".$date->format('m')." AND YEAR(data)=".$year." GROUP BY t ORDER BY data;");
+                    
+        //print chart
+        echo '<div class="chartTest" style="height: 50%; width: 70%; margin:auto;">
+                <div id="curve_chart" class="grafico"></div>
+            </div>';
+    }
+    */
     ?>
 
-    <script>
+    <div class="grafico2">
+        <div id="curve_chart"></div>
+    </div>
 
-        let x = document.getElementsByClassName("chartTest");
-        let y = x;
+    <?php
+        
+        $date = new DateTime(date('Y/m/d H:i:s'));
+        $year = $date->format("Y");
+
+        $res = $db->query("SELECT * FROM `Y$year` where data = (SELECT MAX(data) FROM `Y$year`)")[0];
+        
+        //$res = query("SELECT * FROM `Y$year` where data BETWEEN '".$date->format('Y/m/d')." 00:00:00' and '".$date->format('Y/m/d')." 23:59:59' ORDER BY data desc;")[0];
+    ?>
+
+    <script type="text/javascript" defer>
+        console.log("on");
+
+        let x = document.getElementById("body");
+        let y = document.getElementById("body");
         let z;
         let k;
+
+        
+        <?php 
+            $endDate = new DateTime( $db->query("SELECT MAX(data) as data FROM `Y$year`")[0]["data"] );
+            $startDate = (new DateTime($endDate->format("Y-m-d H:i:s")))->modify("-31 day");
+            $temperaturaMensile = [];
+            $umiditaMensile = [];
+            for($i=30;$i>=0;--$i){
+                $currentDay = (new DateTime($endDate->format("Y-m-d H:i:s")))->modify("-$i day");
+                $res = $db->query("SELECT AVG(umidita) as umiditaMedia, AVG(temperatura) as temperaturaMedia FROM `Y$year` WHERE data= '".$currentDay->format('Y/m/d H:i:s')."';");
+                $temperaturaMensile[] = $res[0]['temperaturaMedia'];
+                $umiditaMensile[] = $res[0]['umiditaMedia'];
+            }
+            
+        ?>
+
 
         google.charts.load('current', {'packages':['corechart']});
         google.charts.setOnLoadCallback(drawChart);
         
+        const giorniSet = ["Lun","Mar","Mer","Gio","Ven","Sab","Dom"];
+        let giorno = <?php echo ($date->format("d"));?>;
+        let giornoSet = giorniSet[<?php echo ($date->format("N")-1);?>]
+        
         function drawChart() {
+            console.log(x.clientWidth);
             if(x.clientWidth/2 <= 441){
-                z = x.clientWidth-25;
-                k = y.clientHeight*6;
+                z = x.clientWidth*0.90;
+                k = y.clientHeight/4;
             }else if(x.clientWidth/2 >= 700){
-                z= x.clientWidth*1;
-                k = y.clientHeight*6;
+                z= x.clientWidth/1.5;
+                k = y.clientHeight/2;
             }else{
-                z= x.clientWidth*1;
-                k = y.clientHeight*6;
+                z= x.clientWidth/1.5;
+                k = y.clientHeight/3;
             }
 
             var data = google.visualization.arrayToDataTable([
-                ['Data', 'Temperatura', 'Umidità', 'Pressione', 'Velocita vento'],
-                <?php 
-                foreach($monthData as $key=>$val) {
-                    
-                    if($key == sizeof($monthData)-1) 
-                        echo "['".$val['t']."', ".$val['temperatura'].", ".$val['umidita'].", ".$val["pressione"].", ".$val['km-h']."]";
-                    else 
-                        echo "['".$val['t']."', ".$val['temperatura'].", ".$val['umidita'].", ".$val["pressione"].", ".$val['km-h']."],";
-                }
-                ?>
-            ]);
-
+                ['Giorno', 'Temperatura', 'Umidità'],
+                ["",         <?php echo (($temperaturaMensile[0]==null)?0:$temperaturaMensile[0])?>, <?php echo (($umiditaMensile[0]==null)?0:$umiditaMensile[0]) ?>],
+                ["",         <?php echo (($temperaturaMensile[1]==null)?0:$temperaturaMensile[1])?>, <?php echo (($umiditaMensile[1]==null)?0:$umiditaMensile[1]) ?>],
+                ["",         <?php echo (($temperaturaMensile[2]==null)?0:$temperaturaMensile[2])?>, <?php echo (($umiditaMensile[2]==null)?0:$umiditaMensile[2]) ?>],
+                ["",         <?php echo (($temperaturaMensile[3]==null)?0:$temperaturaMensile[3])?>, <?php echo (($umiditaMensile[3]==null)?0:$umiditaMensile[3]) ?>],
+                ["",         <?php echo (($temperaturaMensile[4]==null)?0:$temperaturaMensile[4])?>, <?php echo (($umiditaMensile[4]==null)?0:$umiditaMensile[4]) ?>],
+                ["",         <?php echo (($temperaturaMensile[5]==null)?0:$temperaturaMensile[5])?>, <?php echo (($umiditaMensile[5]==null)?0:$umiditaMensile[5]) ?>],
+                ["",         <?php echo (($temperaturaMensile[6]==null)?0:$temperaturaMensile[6])?>, <?php echo (($umiditaMensile[6]==null)?0:$umiditaMensile[6]) ?>],
+                ["",         <?php echo (($temperaturaMensile[7]==null)?0:$temperaturaMensile[7])?>, <?php echo (($umiditaMensile[7]==null)?0:$umiditaMensile[7]) ?>],
+                ["",         <?php echo (($temperaturaMensile[8]==null)?0:$temperaturaMensile[8])?>, <?php echo (($umiditaMensile[8]==null)?0:$umiditaMensile[8]) ?>],
+                ["",        <?php echo (($temperaturaMensile[9]==null)?0:$temperaturaMensile[9])?>, <?php echo (($umiditaMensile[9]==null)?0:$umiditaMensile[9]) ?>],
+                ["",        <?php echo (($temperaturaMensile[10]==null)?0:$temperaturaMensile[10])?>, <?php echo (($umiditaMensile[10]==null)?0:$umiditaMensile[10]) ?>],
+                ["",        <?php echo (($temperaturaMensile[11]==null)?0:$temperaturaMensile[11])?>, <?php echo (($umiditaMensile[11]==null)?0:$umiditaMensile[11]) ?>],
+                ["",        <?php echo (($temperaturaMensile[12]==null)?0:$temperaturaMensile[12])?>, <?php echo (($umiditaMensile[12]==null)?0:$umiditaMensile[12]) ?>],
+                ["",        <?php echo (($temperaturaMensile[13]==null)?0:$temperaturaMensile[13])?>, <?php echo (($umiditaMensile[13]==null)?0:$umiditaMensile[13]) ?>],
+                ["",        <?php echo (($temperaturaMensile[14]==null)?0:$temperaturaMensile[14])?>, <?php echo (($umiditaMensile[14]==null)?0:$umiditaMensile[14]) ?>],
+                ["",        <?php echo (($temperaturaMensile[15]==null)?0:$temperaturaMensile[15])?>, <?php echo (($umiditaMensile[15]==null)?0:$umiditaMensile[15]) ?>],
+                ["",        <?php echo (($temperaturaMensile[16]==null)?0:$temperaturaMensile[16])?>, <?php echo (($umiditaMensile[16]==null)?0:$umiditaMensile[16]) ?>],
+                ["",        <?php echo (($temperaturaMensile[17]==null)?0:$temperaturaMensile[17])?>, <?php echo (($umiditaMensile[17]==null)?0:$umiditaMensile[17]) ?>],
+                ["",        <?php echo (($temperaturaMensile[18]==null)?0:$temperaturaMensile[18])?>, <?php echo (($umiditaMensile[18]==null)?0:$umiditaMensile[18]) ?>],
+                ["",        <?php echo (($temperaturaMensile[19]==null)?0:$temperaturaMensile[19])?>, <?php echo (($umiditaMensile[19]==null)?0:$umiditaMensile[19]) ?>],
+                ["",        <?php echo (($temperaturaMensile[20]==null)?0:$temperaturaMensile[20])?>, <?php echo (($umiditaMensile[20]==null)?0:$umiditaMensile[20]) ?>],
+                ["",       <?php echo (($temperaturaMensile[21]==null)?0:$temperaturaMensile[21])?>, <?php echo (($umiditaMensile[21]==null)?0:$umiditaMensile[21]) ?>],
+                ["",       <?php echo (($temperaturaMensile[22]==null)?0:$temperaturaMensile[22])?>, <?php echo (($umiditaMensile[22]==null)?0:$umiditaMensile[22]) ?>],
+                ["",       <?php echo (($temperaturaMensile[23]==null)?0:$temperaturaMensile[23])?>, <?php echo (($umiditaMensile[23]==null)?0:$umiditaMensile[23]) ?>],
+                ["",       <?php echo (($temperaturaMensile[24]==null)?0:$temperaturaMensile[24])?>, <?php echo (($umiditaMensile[24]==null)?0:$umiditaMensile[24]) ?>],
+                ["",       <?php echo (($temperaturaMensile[25]==null)?0:$temperaturaMensile[25])?>, <?php echo (($umiditaMensile[25]==null)?0:$umiditaMensile[25]) ?>],
+                ["",       <?php echo (($temperaturaMensile[26]==null)?0:$temperaturaMensile[26])?>, <?php echo (($umiditaMensile[26]==null)?0:$umiditaMensile[26]) ?>],
+                ["",       <?php echo (($temperaturaMensile[27]==null)?0:$temperaturaMensile[27])?>, <?php echo (($umiditaMensile[27]==null)?0:$umiditaMensile[27]) ?>],
+                ["",       <?php echo (($temperaturaMensile[28]==null)?0:$temperaturaMensile[28])?>, <?php echo (($umiditaMensile[28]==null)?0:$umiditaMensile[28]) ?>],
+                ["",       <?php echo (($temperaturaMensile[29]==null)?0:$temperaturaMensile[29])?>, <?php echo (($umiditaMensile[29]==null)?0:$umiditaMensile[29]) ?>],
+                [giornoSet+" "+giorno,       <?php echo (($temperaturaMensile[30]==null)?0:$temperaturaMensile[30])?>, <?php echo (($umiditaMensile[30]==null)?0:$umiditaMensile[30]) ?>]
+            ])
 
             var options = {
-                title: 'Resoconto mensile',
+                title: 'Temperatura, Umidità e Pressione Mensile',
                 curveType: 'function',
                 legend: { position: 'bottom' },
                 width: Math.trunc(z),
-                height: Math.trunc(k),
-                vAxis: {
-                    viewWindowMode: 'explicit',
-                    viewWindow: {
-                        min: 0
-                    }
-                }
+                height: Math.trunc(k)
             };
 
             var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
