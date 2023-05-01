07d506603e7481638787ddebb10765d157d9798c
Lorenzoboar <120569374+Lorenzoboar@users.noreply.github.com>
2023-05-01 18:02:26 +0200

    aggiornata la pagina "Chi Siamo"
    Preso tutto il possibile dal sito ufficiale della scuola, ancora work in progress

--- additional/chi_siamo.php
+++ additional/chi_siamo.php
@@ -4,12 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chi siamo</title>
+    <title>Storico</title>
 
     <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Charm">
-    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kalam"> -->
 </head>
 <body>
     
@@ -34,34 +32,63 @@
         ?>
     </nav>
 
-    <h2 class="scuola noiSiamo">
-        ITIS G. Vallauri di Velletri
+    <h2 class="benvenuto noiSiamo">
+        Dalla scuola ITIS Vallauri Velletri:
     </h2>
 
-    <p class="descrizione">
-        Questo progetto è stato sviluppato per rendere fruibile i dati raccolti dalle rilevazioni della nostra stazione meteorologica in modo semplice e veloce, attraverso il Web.
-        Per la creazione della piattaforma, il reperimento e la catalogazione dei dati hanno lavorato gruppi di studenti provenienti dalle classi 3F, 5e e 5G nell'a.s. 2022/2023
-    </p>
+    <h2 class="benvenuto">
+        Questo è un progetto elaborato da 3 classi per illustrare le competenze acquisite.
+    </h2>
+
+    <div class="plusInfo noiSiamo">
+        <h2 class="benvenuto">
+            Per maggiori informazioni sulla scuola:
+        </h2>
+
+        <a href="https://www.itisvallauri.edu.it">
+            <h2 class="benvenuto">| Sito della Scuola |</h2>
+        </a>
+    </div>
 
     <!-- mappa -->
 
-    <h2 class="benvenuto"> 
-        Qui è dove ci troviamo: 
+    <h2 class="benvenuto noiSiamo"> 
+        Posizione della Scuola: 
     </h2>
 
     <div class="google-maps">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2980.0416283289383!2d12.775315615372184!3d41.676444379238745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13259d347813ad7d%3A0x973948516bf1e19f!2sITIS%20Giancarlo%20Vallauri!5e0!3m2!1sit!2sit!4v1678190626853!5m2!1sit!2sit" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 
-    <div class="spazio sfondotrasparente">
+    <div class="spazio sfondotrasparente sfondOscurato">
         <div class="sottoSpazio1">
-            <h2 class="scuola">
+            <h2 class="benvenuto">
                 La Storia della scuola
             </h2>
             <hr>
-            <p class="descrizione">
+            <h2 class="storia">
                 La scuola viene istituita nell' A. S. 1960 /61 come succursale dell'I.T.I.S. "E. Fermi" di Roma all'epoca esistevano solo tre sezioni: la sezione A di 37 alunni la sezione B di 37 alunni la sezione C di 39 alunni le quali erano seguite dal Prof. Diana Umberto. il quale è stato il coordinatore con la partecipazione dell' I.T.I.S. E. Fermi di Roma per l' avvio e la gestione iniziale della scuola. Tutti i comuni a sud di Roma come ad esempio i Castelli , Colleferro ecc. insieme ad alcuni comuni della provincia di Latina, fino a giungere a Terracina, rappresentano il bacino d 'utenza dell' attuale I.T.I.S. "Giancarlo Vallauri" di Velletri. Inizialmente l'Istituto si appoggiò presso l' I.T.C.G. "Cesare Battisti" , per passare poi dal palazzo comunale ad una villetta non troppo distante l'ospedale di Velletri. Nel mese di novembre dell'anno 1965 l'I.T.I.S. Vallauri, si trasferì definitivamente presso la sede attuale di via Salvo D' Acquisto. Nell' A. S. 1968 / 69 divenne istituto autonomo e prese per l'appunto il nome dell' ingegnere "Giancarlo Vallauri". Dalla sua autonomia ad oggi, più di cinquemila sono stati gli studenti diplomati. Questo perché il numero delle sezioni e degli alunni è progressivamente cresciuto nel corso del tempo; fino a raggiungere, un numero complessivo di 53 classi, 14 di esse sono state ospitate nella sede staccata di via Paolina a Velletri. La scuola dispone inoltre di diversi laboratori, dove gli alunni possono predisporre numerose esperienze. Al tempo stesso, dopo l'entrata in vigore dei bacini di utenza, (DISTRETTO N°39/42) si è precisata e definita l'area geografica dell'I.T.I.S. "G. Vallauri": gli allievi provengono ,infatti, prevalentemente da Lariano, Genzano, Albano, Ariccia, Lanuvio, Cecchina, S.Maria delle Mole, Castel Gandolfo, Ciampino, Cisterna e Artena oltre che , come ovvio, dalla stessa Velletri. Alla iniziale specializzazione in Elettronica Industriale si sono affiancati, più di recente nuovi corsi (INFORMATICA e LICEO SCIENTIFICO TECNOLOGICO), ciò nell'intento di rispondere con sempre maggiore efficacia alla domanda e alle legittime esigenze del mondo del lavoro che, alla scuola chiede un continuo sforzo di aggiornamento per adeguare i futuri periti all'apprendimento e all'uso delle nuove tecnologie.
-            </p>
+            </h2>
+            <div id="timeLine">
+                <h2 class="benvenuto">---Time Line-></h2>
+                <div class="subTimeLine">
+                    <div class="subTimeLineDescription">
+                        <h4>Settembre 1960</h4>
+                        <h2>Fondazione della scuola</h2>
+                        <h3>La scuola viene istituita nell' A. S. 1960 /61 come succursale dell'I.T.I.S. "E. Fermi" di Roma all'epoca esistevano solo tre sezioni.</h3>
+                    </div>
+                    <div class="subTimeLineDescription">
+                        <h4>Novembre 1965</h4>
+                        <h2>Trasferimento sede</h2>
+                        <h3>Nel mese di novembre dell'anno 1965 l'I.T.I.S. Vallauri, si trasferì definitivamente presso la sede attuale di via Salvo D' Acquisto.</h3>
+                    </div>
+                    <div class="subTimeLineDescription">
+                        <h4>Settembre 1968</h4>
+                        <h2>Ridenominazione istituto</h2>
+                        <h3>Nell' A. S. 1968 / 69 divenne istituto autonomo e prese per l'appunto il nome dell' ingegnere "Giancarlo Vallauri".</h3>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="sottoSpazio2">
             <img src="../img/scuola1.jpg" alt="IMG-scuola">
@@ -69,39 +96,98 @@
         </div>
     </div>
 
-    <div class="spazio">
-        <h2 class="benvenuto"> 
-            Partecipanti al Progetto
-        </h2>
-        <hr>
-        <h5 class="benvenuto" style="color:#00FF00;">
-            Somma Francesco [Back-End, Front-End, Database]
-        </h5>
-        <br>
-        <h5 class="benvenuto" style="color:#00FF00;">
-            Fonti Luca [Back-End, Front-End, Database]
-        </h5>
-        <br>
-        <h5 class="benvenuto" style="color:#00FF00;">     
-            Boaretto Lorenzo [Back-End, Front-End, Grafica]
-        </h5>
-        <br>
-        <h5 class="benvenuto" style="color:#0000FF;">
-            Cipolla Emilio [Front-end, Grafica]
-        </h5>
-        <br>
-        <h5 class="benvenuto" style="color:#0000FF;">
-            Pietrosanti Francesco [Front-End, Grafica, Presentazione]
-        </h5>
-        <br>
-        <h5 class="benvenuto" style="color:#FF0000;">
-            Imbastari Riccardo [Grafica, Presentazione]
-        </h5>
-        <br>
-        <h5 class="benvenuto" style="color:#FF0000;">
-            Fruncillo Carmine [Idee]
-        </h5>
-    </div>
+    <div id="partecipanti">
 
+        <div class="spazio sfondOscurato">
+            <h2 class="benvenuto"> 
+                Partecipanti al Progetto
+                [Professori]
+            </h2>
+            <hr>
+
+            <h5 class="benvenuto specialText1">
+                Bonifazi Andrea [Informatica]
+            </h5>
+            <h5 class="benvenuto specialText2">
+                Rispoli Maria [Scienze e Biologia]
+            </h5>
+        </div>
+
+        <div id="sottoPartecipanti">
+
+            <div class="spazio sfondOscurato">
+                <h2 class="benvenuto"> 
+                    Partecipanti al Progetto
+                    [Informatica]
+                </h2>
+                <hr>
+                
+                <h5 class="benvenuto" style="color:#00FF00;">
+                    Somma Francesco [Back-End, Front-End, Database]
+                </h5>
+                
+                <h5 class="benvenuto" style="color:#00FF00;">
+                    Fonti Luca [Back-End, Front-End, Database]
+                </h5>
+                
+                <h5 class="benvenuto" style="color:#00FF00;">     
+                    Boaretto Lorenzo [Back-End, Front-End, Grafica]
+                </h5>
+                
+                <h5 class="benvenuto" style="color:#0000FF;">
+                    Cipolla Emilio [Front-end, Grafica]
+                </h5>
+                
+                <h5 class="benvenuto" style="color:#0000FF;">
+                    Pietrosanti Francesco [Front-End, Grafica, Presentazione]
+                </h5>
+                
+                <h5 class="benvenuto" style="color:#FF0000;">
+                    Imbastari Riccardo [Grafica, Presentazione]
+                </h5>
+                
+                <h5 class="benvenuto" style="color:#FF0000;">
+                    Fruncillo Carmine [Idee]
+                </h5>
+            </div>
+
+            <div class="spazio sfondOscurato">
+            <h2 class="benvenuto"> 
+                    Partecipanti al Progetto
+                    [Scientifico]
+                </h2>
+                <hr>
+                
+                <h5 class="benvenuto" style="color:#000000;">
+                    . []
+                </h5>
+                
+                <h5 class="benvenuto" style="color:#000000;">
+                    . []
+                </h5>
+                
+                <h5 class="benvenuto" style="color:#000000;">     
+                    . []
+                </h5>
+                
+                <h5 class="benvenuto" style="color:#000000;">
+                    . []
+                </h5>
+                
+                <h5 class="benvenuto" style="color:#000000;">
+                    . []
+                </h5>
+                
+                <h5 class="benvenuto" style="color:#000000;">
+                    . []
+                </h5>
+                
+                <h5 class="benvenuto" style="color:#000000;">
+                    . []
+                </h5>
+            </div>
+
+        </div>
+    </div>
 </body>
 </html>
