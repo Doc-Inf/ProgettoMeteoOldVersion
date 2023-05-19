bae8b2c12076626c135f8e471a3b54dbace384ce
SirFoscolo2 <120178386+SirFoscolo2@users.noreply.github.com>
2023-05-20 00:30:20 +0200

    Delete safe directory

--- safe/DB/DB.php
+++
@@ -1,8 +0,0 @@
-<?php
-    interface DB{
-        public function getConnection();
-        public function query(string $sql,$params=[]);
-        public function dmlCommand(string $sql,$params=[]);
-    }
-
-?>
